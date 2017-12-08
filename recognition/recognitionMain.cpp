62e5c901ff54aa5e6868db9f37c42c77dba57ec0
Luyao Hou <hly.charles@gmail.com>
2017-12-07 19:05:43 -0500

    output distance

--- recognition/recognitionMain.cpp
+++ recognition/recognitionMain.cpp
@@ -2,14 +2,18 @@
 #include "../resample/resample.h"
 
 
-void nomi(int inputImg[400], int result[2]) {
+void nomi(int inputImg[400], int result[10]) {
 #pragma HLS INTERFACE axis port=inputImg
 #pragma HLS INTERFACE axis port=result
 
 	int dists[100];
 	int faceIndex = recognizeImage(inputImg, dists);
 
-	for (int i = 0; i < 2; i++) {
-		result[i] = faceIndex;
+	for (int i = 0; i < 10; i++) {
+		if (i < 2) {
+			result[i] = faceIndex;
+		} else {
+			result[i] = dists[i - 2];
+		}
 	}
 }
