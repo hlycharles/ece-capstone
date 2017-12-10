9ef95f1c203f882df4ba863746b9b0dcf0dfb74d
Luyao Hou <hly.charles@gmail.com>
2017-12-10 17:23:54 -0500

    clean up

--- recognition/recognitionModule.cpp
+++ recognition/recognitionModule.cpp
@@ -4,26 +4,12 @@
 const int imgSize = 20;
 
 int recognition(int inImg[], int r, int c, int dists[]) {
-// #pragma HLS INTERFACE axis port=inputImg
-// #pragma HLS INTERFACE axis port=result
 
 	int resizedImg[400];
-
-	// int i;
 
 	resample(r, c, inImg, resizedImg);
 
 	int faceIndex = recognizeImage(resizedImg, dists);
-
-	// leave blank space for fault tolerance
-	/*
-	for (i = 0; i < 30; i++) {
-		if (i < 18) {
-			result[i] = faceIndex;
-		} else {
-			result[i] = dists[i % 6];
-		}
-	} */
 
 	return faceIndex;
 }
